5b7cd2d97b0256740bfd03bd55178cf593613135
laurpaik <laurpaik@gmail.com>
2017-07-24 13:29:33 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { CodemirrorModule } from 'ng2-codemirror';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CodemirrorModule,
     FormsModule,
     HttpModule
   ],
