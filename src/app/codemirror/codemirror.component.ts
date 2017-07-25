e4e0e2b13d5ad6be1463cfe0f6a4d7eb0f1e4b54
laurpaik <laurpaik@gmail.com>
2017-07-25 15:18:45 -0400

    Add buttons to run and clear
    - Add theme for cool style

--- src/app/codemirror/codemirror.component.ts
+++ src/app/codemirror/codemirror.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
-import 'codemirror/mode/javascript/javascript';
+import { Component, OnInit } from '@angular/core';
+import { CodemirrorModule } from 'ng2-codemirror';
 
 @Component({
   selector: 'text-editor',
   templateUrl: './codemirror.component.html',
   styleUrls: ['./codemirror.component.css']
 })
-export class CodemirrorComponent {
-  code = `heyhey`;
+export class CodemirrorComponent implements OnInit {
+  public starterCode;
+  code = `\/\/\ Type \`start\` to begin.`;
   config = {
-    mode: 'javascript',
+    mode: 'shell',
     lineWrapping: false,
     lineNumbers: true,
-    theme: 'monokai'
+    theme: 'vibrant-ink'
   };
+  ngOnInit() {
+    this.starterCode = this.code;
+  }
+  onRun() {
+    if (this.code.includes('start')) {
+      console.log(this.starterCode);
+      this.code = 'testing';
+    }
+    else {
+      console.log('RUN');
+    }
+  }
+  onClear () {
+    console.log('CLEAR');
+    this.code = '';
+  }
 }
